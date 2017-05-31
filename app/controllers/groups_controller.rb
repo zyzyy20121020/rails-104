3dd5c2e231321d9b4faf2663807cee4ac999e7a3
ReadingPig <zyzpop830203@163.com>
2017-05-31 20:17:04 +0800

    implement groups@show

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -6,5 +6,22 @@ class GroupsController < ApplicationController
   def new
     @group = Group.new
   end
-  
+
+  def show
+    @group = Group.find(params[:id])
+  end
+
+  def create
+    @group = Group.new(group_params)
+    @group.save
+
+    redirect_to groups_path
+  end
+
+  private
+
+  def group_params
+    params.require(:group).permit(:title, :description)
+  end
+
 end
