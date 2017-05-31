c2e06c4e9ff60387f9f0fbfd9b01303d6f861121
ReadingPig <zyzpop830203@163.com>
2017-05-31 23:57:41 +0800

    use before_action to find_group_and_check_permission

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -59,6 +59,14 @@ class GroupsController < ApplicationController
 
   private
 
+  def find_group_and_check_permission
+    @group = Group.find(params[:id])
+
+    if current_user != @group.user
+      redirect_to root_path, alert: "You have no permission"
+    end
+  end  
+
   def group_params
     params.require(:group).permit(:title, :description)
   end
