d12a73f221142068391625043e299d68446601bf
ReadingPig <zyzpop830203@163.com>
2017-06-01 19:20:32 +0800

    implement is_member_of?(group)

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,11 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
   has_many :groups
   has_many :posts
+
+  has_many :group_relationships
+  has_many :participated_groups, :through => :group_relationships, :source => :group
+
+  def is_member_of?(group)
+    participated_groups.include?(group)
+  end
 end
